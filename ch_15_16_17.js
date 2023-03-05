00e59e65775d5f6911f9be28e0fe84685e19153c
Oba1d-Khan <mobaidkhan2@gmail.com>
2023-03-05 12:38:04 +0500

    chp -15

--- ch_15_16_17.js
+++ ch_15_16_17.js
@@ -1,17 +1,26 @@
-
 var courses = [];
-var course1 = prompt("Course 1 : "  );
-var course2 = prompt("Course 2 : "  );
-var course3 = prompt("Course 3 : "  );
-var course4 = prompt("Course 4 : "  );
-var course5 = prompt("Course 5 : "  );
-courses.push(course1)
-courses.push(course2)
-courses.push(course3)
-courses.push(course4)
-courses.push(course5)
-// alert(courses);
-console.log(courses)
-var courseEdit = prompt(courses.splice());
-// alert(courses);
-console.log(courses)
+var course_1Value = prompt("Enter Course 1 ! "  );
+var course_2Value = prompt("Enter Course 2 ! "  );
+var course_3Value = prompt("Enter Course 3 ! "  );
+// var course_4Value = prompt("Enter Course 4 ! "  );
+// var course_5Value = prompt("Enter Course 5 ! "  );
+courses.push(course_1Value);
+courses.push(course_2Value);
+courses.push(course_3Value);
+// courses.push(course_4Value);
+// courses.push(course_5Value);
+alert(courses);
+
+// var course1 = prompt("Course 1 : " ,course_1Value );
+// var course2 = prompt("Course 2 : " ,course_2Value );
+// var course3 = prompt("Course 3 : " ,course_3Value );
+// var course4 = prompt("Course 4 : " ,course_4Value );
+// var course5 = prompt("Course 5 : " ,course_5Value );    // Step-04 done
+
+var course_1Edit = prompt(course,course_1Value);
+course1 = prompt(course1 ,course_1Edit );
+// course2 = prompt(course2 ,course_2Value );
+
+// var course_2ValueEdit = prompt("Splice : " , courses.splice());
+
+alert(courses);
